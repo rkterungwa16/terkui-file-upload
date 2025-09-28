fc8e919018a1f4261df0f380e04b7d5c21dbe21c
rkterungwa16 <terungwakombol@gmail.com>
2025-09-28 07:04:13 +0100

    modify card design update card header and add card body

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,6 +10,7 @@ import CircularProgressBar from "../src/circular-progress-bar";
 import { Layout } from "../src/layout";
 
 import styles from "../src/styles.module.css";
+import { UploadIcon } from "../src/icons/upload-icon";
 
 const CLOUD_NAME = "doy0uyv63";
 const CLOUD_URL = `https://api.cloudinary.com/v1_1/${CLOUD_NAME}/image/upload`;
@@ -21,123 +22,135 @@ export default function Home() {
   return (
     <Layout>
       <div className={styles.StyledCard}>
-        <div className={styles.StyledPageHeaderWrapper}>
-          <h3>Upload files</h3>
+        <div className={styles.CardHeader}>
+          <div className={styles.CardHeaderIconWrapper}>
+            <UploadIcon />
+          </div>
+          <div className={styles.CardHeaderTextWrapper}>
+            <h5 className={styles.Text__h5}>Upload files</h5>
+            <h6 className={styles.Text__h6}>
+              Select and upload the files of your choice
+            </h6>
+          </div>
         </div>
-        <FileUploadManager
-          url={CLOUD_URL}
-          method={"POST"}
-          headers={{ "X-Requested-With": "XMLHttpRequest" }}
-          inputComponent={<FileUploadInput id={id} name={name} />}
-          uploadAreaComponent={
-            <FileUploadArea
-              style={{
-                display: "flex",
-                flexDirection: "column",
-                alignItems: "center",
-                width: "400px",
-                height: "250px",
-                border: "1px dashed #6F6F6F",
-                padding: "1rem",
-                background: "#f3f1f2",
-                borderRadius: "1rem",
-              }}
-            >
-              <h4 style={{ paddingTop: "1rem", paddingBottom: "0.5rem" }}>
-                <b>Drag and Drop files here</b>
-              </h4>
-              <p style={{ paddingTop: "0.5rem", paddingBottom: "1rem" }}>OR</p>
-
-              <label
-                className={`${styles.StyledPrimaryButton} ${styles.Button}`}
-                role="button"
-                htmlFor={name}
+        <div className={styles.CardBody}>
+          <FileUploadManager
+            url={CLOUD_URL}
+            method={"POST"}
+            headers={{ "X-Requested-With": "XMLHttpRequest" }}
+            inputComponent={<FileUploadInput id={id} name={name} />}
+            uploadAreaComponent={
+              <FileUploadArea
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                  width: "100%",
+                  height: "250px",
+                  border: "1px dashed #CBD0DC",
+                  padding: "1rem",
+                  background: "#ffffff",
+                  borderRadius: "1rem",
+                }}
               >
-                upload
-              </label>
-            </FileUploadArea>
-          }
-        >
-          {({ files, method, url, headers }) => {
-            if (files.length) {
-              return (
-                <div
-                  style={{
-                    background: "#f3f1f2",
-                    width: "100%",
-                    marginTop: "1rem",
-                    padding: "0.6rem",
-                    borderRadius: "1rem",
-                  }}
+                <h4 style={{ paddingTop: "1rem", paddingBottom: "0.5rem" }}>
+                  <b>Drag and Drop files here</b>
+                </h4>
+                <p style={{ paddingTop: "0.5rem", paddingBottom: "1rem" }}>
+                  OR
+                </p>
+
+                <label
+                  className={`${styles.StyledPrimaryButton} ${styles.Button}`}
+                  role="button"
+                  htmlFor={name}
                 >
-                  {files.map((_file) => (
-                    <FileUpload
-                      key={_file.fileId}
-                      method={method}
-                      url={url}
-                      headers={headers}
-                      file={_file}
-                      auto
-                    >
-                      {({ requestState, startUpload, events, progress }) => (
-                        <>
-                          {events?.upload_ready?.fileDataUrl && (
-                            <>
-                              {/* {console.log("events____", events)}
+                  upload
+                </label>
+              </FileUploadArea>
+            }
+          >
+            {({ files, method, url, headers }) => {
+              if (files.length) {
+                return (
+                  <div
+                    style={{
+                      background: "#f3f1f2",
+                      width: "100%",
+                      marginTop: "1rem",
+                      padding: "0.6rem",
+                      borderRadius: "1rem",
+                    }}
+                  >
+                    {files.map((_file) => (
+                      <FileUpload
+                        key={_file.fileId}
+                        method={method}
+                        url={url}
+                        headers={headers}
+                        file={_file}
+                        auto
+                      >
+                        {({ requestState, startUpload, events, progress }) => (
+                          <>
+                            {events?.upload_ready?.fileDataUrl && (
+                              <>
+                                {/* {console.log("events____", events)}
                                   {console.log("status____", requestState)}
                                   {console.log("progress____", progress)} */}
 
-                              <div
-                                className={styles.StyledCard}
-                                style={{
-                                  position: "relative",
-                                  display: "flex",
-                                  alignItems: "center",
-                                  flexDirection: "row",
-                                  marginBottom: "0.5rem",
-                                }}
-                              >
-                                <Image
-                                  src={events.upload_ready.fileDataUrl}
-                                  alt={_file.name}
-                                  width={50}
-                                  height={50}
-                                  style={{
-                                    borderRadius: "6px",
-                                  }}
-                                />
                                 <div
+                                  className={styles.StyledCard}
                                   style={{
-                                    marginLeft: "0.5rem",
+                                    position: "relative",
                                     display: "flex",
-                                    flexDirection: "column",
-                                    justifyContent: "center",
+                                    alignItems: "center",
+                                    flexDirection: "row",
+                                    marginBottom: "0.5rem",
                                   }}
                                 >
-                                  <span
+                                  <Image
+                                    src={events.upload_ready.fileDataUrl}
+                                    alt={_file.name}
+                                    width={50}
+                                    height={50}
                                     style={{
-                                      fontSize: "14px",
-                                      fontWeight: "bold",
-                                      marginBottom: "0.5rem",
+                                      borderRadius: "6px",
+                                    }}
+                                  />
+                                  <div
+                                    style={{
+                                      marginLeft: "0.5rem",
+                                      display: "flex",
+                                      flexDirection: "column",
+                                      justifyContent: "center",
                                     }}
                                   >
-                                    {_file.name}
-                                  </span>
-                                  {requestState &&
-                                    events[requestState] &&
-                                    progress.percent !== 1 && (
-                                      <span
-                                        style={{
-                                          fontSize: "14px",
-                                          color: "#a4aeb6",
-                                          fontWeight: "bold",
-                                        }}
-                                      >
-                                        {events.upload_progress.timeRemaining} s
-                                      </span>
-                                    )}
-                                </div>
-                                {/* <button
+                                    <span
+                                      style={{
+                                        fontSize: "14px",
+                                        fontWeight: "bold",
+                                        marginBottom: "0.5rem",
+                                      }}
+                                    >
+                                      {_file.name}
+                                    </span>
+                                    {requestState &&
+                                      events[requestState] &&
+                                      progress.percent !== 1 && (
+                                        <span
+                                          style={{
+                                            fontSize: "14px",
+                                            color: "#a4aeb6",
+                                            fontWeight: "bold",
+                                          }}
+                                        >
+                                          {events.upload_progress.timeRemaining}{" "}
+                                          s
+                                        </span>
+                                      )}
+                                  </div>
+                                  {/* <button
                                     style={{
                                       position: "absolute",
                                       display: "flex",
@@ -171,42 +184,43 @@ export default function Home() {
                                     />
                                   </button> */}
 
-                                {requestState &&
-                                  events[requestState] &&
-                                  progress.percent !== 1 && (
-                                    <div
-                                      style={{
-                                        position: "absolute",
-                                        display: "flex",
-                                        alignItems: "center",
-                                        justifyContent: "center",
-                                        right: "0.5rem",
-                                      }}
-                                    >
-                                      <CircularProgressBar
-                                        width="45"
-                                        height="48"
-                                        viewBox="0 0 45 48"
-                                        radius={16}
-                                        firstArcEndAngle={
-                                          progress.percent * 359.9
-                                        }
-                                      />
-                                    </div>
-                                  )}
-                              </div>
-                            </>
-                          )}
-                        </>
-                      )}
-                    </FileUpload>
-                  ))}
-                </div>
-              );
-            }
-            return null;
-          }}
-        </FileUploadManager>
+                                  {requestState &&
+                                    events[requestState] &&
+                                    progress.percent !== 1 && (
+                                      <div
+                                        style={{
+                                          position: "absolute",
+                                          display: "flex",
+                                          alignItems: "center",
+                                          justifyContent: "center",
+                                          right: "0.5rem",
+                                        }}
+                                      >
+                                        <CircularProgressBar
+                                          width="45"
+                                          height="48"
+                                          viewBox="0 0 45 48"
+                                          radius={16}
+                                          firstArcEndAngle={
+                                            progress.percent * 359.9
+                                          }
+                                        />
+                                      </div>
+                                    )}
+                                </div>
+                              </>
+                            )}
+                          </>
+                        )}
+                      </FileUpload>
+                    ))}
+                  </div>
+                );
+              }
+              return null;
+            }}
+          </FileUploadManager>
+        </div>
       </div>
     </Layout>
   );
